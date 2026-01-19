a5af9c830c59424da5bead6da7efcd3f4dacd021
Amarsanaa_Byambaa <amarsanaabymbaa99@gmail.com>
2026-01-19 15:30:38 +0800

    Update new_dashboard.py

--- dashboards/new_dashboard.py
+++ dashboards/new_dashboard.py
@@ -417,13 +417,10 @@ with right:
             ]
         )
 
-        chart = lines + points
-        
         st.altair_chart(
-            chart.properties(height=520).interactive(),
+            lines.properties(height=520).interactive(),
             use_container_width=True
         )
-
 
     def compute_group_kpis(df, indicators):
         stats = []
