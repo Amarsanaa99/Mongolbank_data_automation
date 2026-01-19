607aa0365b670859a877f68782f369560430966c
Amarsanaa_Byambaa <amarsanaabymbaa99@gmail.com>
2026-01-19 17:29:27 +0800

    Update new_dashboard.py

--- dashboards/new_dashboard.py
+++ dashboards/new_dashboard.py
@@ -418,7 +418,7 @@ with right:
         )
 
         st.altair_chart(
-            lines.properties(height=400).interactive(),
+            lines.properties(height=350).interactive(),
             use_container_width=True
         )
 
