160707b8c4312c8c9cc6b5f38644dd7a39e73bb6
Amarsanaa_Byambaa <amarsanaabymbaa99@gmail.com>
2026-01-23 02:17:48 +0800

    Update new_dashboard.py

--- dashboards/new_dashboard.py
+++ dashboards/new_dashboard.py
@@ -649,7 +649,7 @@ with right:
             )
             .properties(
                 # ✅ ЯГ ӨМНӨХ ШИГЭЭ PADDING
-                padding={"left": 20, "top": 20, "right": 15, "bottom": 50}
+                padding={"left": 0, "top": 20, "right": 15, "bottom": 20}
             )
             .configure_view(
                 strokeWidth=0
