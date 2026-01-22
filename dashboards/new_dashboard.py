3108d83c5c95997b7fc99f0e066fec9f182c751d
Amarsanaa_Byambaa <amarsanaabymbaa99@gmail.com>
2026-01-23 02:49:51 +0800

    Update new_dashboard.py

--- dashboards/new_dashboard.py
+++ dashboards/new_dashboard.py
@@ -600,7 +600,7 @@ with right:
             )
             .properties(
                 height=400,  # ✅ ЯГ ӨМНӨХ ШИГЭЭ 400
-                width=1000
+                width=800
             )
             .interactive()
         )
