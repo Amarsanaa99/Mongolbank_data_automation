68eef03ea95a994eb24a9a6e6bce28e3369a93a4
Amarsanaa_Byambaa <amarsanaabymbaa99@gmail.com>
2026-01-24 22:16:51 +0800

    Update new_dashboard.py

--- dashboards/new_dashboard.py
+++ dashboards/new_dashboard.py
@@ -687,7 +687,7 @@ with right:
             )
             .properties(
                 height=60,
-                width=800
+                width=780
             )
             # ✅ MINI CHART ДЭЭР PAN ХИЙХ БОЛОМЖТОЙ (WINDOW-Г ЧИРЖ БАЙРЛУУЛАХ)
             .add_params(mini_brush)
